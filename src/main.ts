e23221d12b9b6108f521258275ccb705697ad29f
jajetloh <jajetloh@gmail.com>
2024-04-09 03:54:49 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,8 @@ import 'zone.js';
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular!!!
-    </a>
-  `,
+  templateUrl: './main.html',
+  styleUrl: './main.css',
 })
 export class App {
   name = 'Angular';
